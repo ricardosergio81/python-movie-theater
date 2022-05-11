664f0e1b363c9f0fa75bab4cbf14478ac4e8c50a
Ricardo Rosa <ricardosergio81@gmail.com>
2022-05-11 03:26:42 -0300

    feat(movie_theaters): test coverage 100%

---
+++ movie-theater-service/app/tests/__init__.py
@@ -0,0 +1,2 @@
+import os
+os.environ["DATABASE_URI"] = "postgresql://movie_theaters_db_username:movie_theaters_db_password@localhost:5435/movie_theaters_db_dev"
---
+++ movie-theater-service/app/tests/test_main.py
@@ -0,0 +1,91 @@
+import pytest
+from starlette.testclient import TestClient
+from app.main import app
+from fastapi import FastAPI
+from typing import Generator
+from ..api.database import Base, engine, SessionLocal
+
+
+@pytest.fixture(scope="session", autouse=True)
+def db() -> Generator:
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    yield SessionLocal()
+
+
+# Create a new application for testing
+@pytest.fixture
+def app() -> FastAPI:
+    from app.main import app
+    return app
+
+
+@pytest.fixture()
+def client(app: FastAPI):
+    with TestClient(app) as client:  # context manager will invoke startup event
+        yield client
+
+
+def test_post_movie_theater(client: TestClient):
+    response = client.post(
+        "/api/v1/movies-theater/",
+        json={"name": "Ricardo Rosa"},
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data["name"] == "Ricardo Rosa"
+    assert "id" in data
+    cast_id = data["id"]
+
+    response = client.get(f"/api/v1/movies-theater/{cast_id}/")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["name"] == "Ricardo Rosa"
+    assert data["id"] == cast_id
+
+
+def test_get_movie_theater(client: TestClient):
+    cast_id = 1
+    response = client.get(f"/api/v1/movies-theater/{cast_id}/")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["name"] == "Ricardo Rosa"
+    assert data["id"] == cast_id
+
+
+def test_get_all_cast(client: TestClient):
+    cast_id = 1
+    response = client.get(f"/api/v1/movies-theater/")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data[0]["name"] == "Ricardo Rosa"
+    assert data[0]["id"] == cast_id
+
+
+def test_put_cast(client: TestClient):
+    cast_id = 1
+    response = client.put(
+        f"/api/v1/movies-theater/{cast_id}/",
+        json={"name": "Name changed"}
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["name"] == "Name changed"
+    assert "id" in data
+    assert data["id"] == cast_id
+
+
+def test_put_cast_id_not_found(client: TestClient):
+    cast_id = 9999999
+    response = client.put(
+        f"/api/v1/movies-theater/{cast_id}/",
+        json={"name": "Name changed"}
+    )
+    assert response.status_code == 404, response.text
+
+
+def test_get_cast_not_found(client: TestClient):
+    cast_id = 9999999
+    response = client.get(f"/api/v1/movies-theater/{cast_id}/")
+    assert response.status_code == 404, response.text
